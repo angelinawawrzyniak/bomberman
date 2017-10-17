5814c4ae8698d9b91345b947152781554be89f29
Angelina Wawrzyniak <angelina.wawrzyniak92@gmail.com>
2017-10-17 23:00:34 +0200

    Bombs: - taking life from user if in destruction area - adding points for destroyed bricks - creating portal
    Context:
    - all global variables moved to one place

--- game.py
+++ game.py
@@ -18,7 +18,7 @@ class Board:
             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'],
         ]
 
-    def draw(self, graphic_buffer):
+    def draw(self, graphic_buffer, _context):
         for index_y in range(len(self.fields)):
             for index_x in range(len(self.fields[index_y])):
                 if self.fields[index_y][index_x] == 'o':
@@ -51,7 +51,7 @@ class User:
                 self.x = x
                 break
 
-    def draw(self, graphic_buffer):
+    def draw(self, graphic_buffer, _context):
         graphic_buffer[self.y][self.x] = 'U'
 
     def is_brick_there(self, bricks, y, x):
@@ -60,34 +60,36 @@ class User:
                 return True
         return False
 
-    def make_step(self, board, bricks):
+    def make_step(self, context):
         chosen_direction = input('w/a/s/d/space')
         if chosen_direction == 'a':
             new_x = self.x - self.step
-            if not board.is_field_occupied(self.y, new_x) and not self.is_brick_there(bricks, self.y, new_x):
+            if not context.board.is_field_occupied(self.y, new_x) and not self.is_brick_there(context.bricks, self.y, new_x):
                 self.x -= self.step
         elif chosen_direction == 'd':
             new_x = self.x + self.step
-            if not board.is_field_occupied(self.y, new_x) and not self.is_brick_there(bricks, self.y, new_x):
+            if not context.board.is_field_occupied(self.y, new_x) and not self.is_brick_there(context.bricks, self.y, new_x):
                 self.x += self.step
         elif chosen_direction == 'w':
             new_y = self.y - self.step
-            if not board.is_field_occupied(new_y, self.x) and not self.is_brick_there(bricks, new_y, self.x):
+            if not context.board.is_field_occupied(new_y, self.x) and not self.is_brick_there(context.bricks, new_y, self.x):
                 self.y -= self.step
         elif chosen_direction == 's':
             new_y = self.y + self.step
-            if not board.is_field_occupied(new_y, self.x) and not self.is_brick_there(bricks, new_y, self.x):
+            if not context.board.is_field_occupied(new_y, self.x) and not self.is_brick_there(context.bricks, new_y, self.x):
                 self.y += self.step
         elif chosen_direction == ' ':
-            bombs.append(Bomb(user))
+            context.bombs.append(Bomb(context.user))
 
 
 class Brick:
 
+    REWARD = 10
+
     def __init__(self, board, user, bricks):
         self.set_up_brick(board, user, bricks)
 
-    def draw(self, graphic_buffer):
+    def draw(self, graphic_buffer, _context):
         graphic_buffer[self.y][self.x] = '|'
 
     def is_field_occupied(self, bricks, y, x):
@@ -112,6 +114,16 @@ class Brick:
                 break
 
 
+class Portal:
+
+    def __init__(self, y, x):
+        self.y = y
+        self.x = x
+
+    def draw(self, graphic_buffer, _context):
+        graphic_buffer[self.y][self.x] = 'P'
+
+
 class Bomb:
 
     def __init__(self, user):
@@ -119,66 +131,87 @@ class Bomb:
         self.x = user.x
         self.time = 4
 
-    def draw(self, graphic_buffer,bombs, user):
+    def draw(self, graphic_buffer, context):
         graphic_buffer[self.y][self.x] = '@'
-        for bomb in bombs:
-            if (self.y, self.x) != (user.y, user.x):
+        for bomb in context.bombs:
+            if (self.y, self.x) != (context.user.y, context.user.x):
                 graphic_buffer[self.y][self.x] = 'B'
 
-    def make_step(self, bombs, dead_list, bricks):
+    def make_step(self, context):
         if self.time > 0:
             self.time -= 1
-        if self.time == 0 and self not in dead_list:
-            dead_list.append(self)
+        if self.time == 0 and self not in context.dead_list:
+            context.dead_list.append(self)
             offsets = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1),]
-            for brick in bricks:
-                if brick not in dead_list:
+            for brick in context.bricks:
+                if brick not in context.dead_list:
                     for (offset_y, offset_x) in offsets:
                         if (brick.y, brick.x) == (self.y + offset_y, self.x + offset_x):
-                            dead_list.append(brick)
+                            context.dead_list.append(brick)
+                            self.add_point(context.user, brick)
+                            if context.portal is None:
+                                context.portal = Portal(brick.y, brick.x)
+
+            for (offset_y, offset_x) in offsets:
+                if (context.user.y, context.user.x) == (self.y + offset_y, self.x + offset_x):
+                    context.user.life -= 1
+                    break
+
+    def add_point(self, user, destroyed_objects):
+        user.points += destroyed_objects.REWARD
 
 
-board = Board()
-user = User(1, board)
-bricks = []
-bombs = []
-dead_list =[]
-for _ in range(20):
-    bricks.append(Brick(board, user, bricks))
+class Context:
+
+    def __init__(self):
+        self.board = Board()
+        self.user = User(1, self.board)
+        self.bricks = []
+        self.portal = None
+        self.bombs = []
+        self.dead_list =[]
+        for _ in range(20):
+            self.bricks.append(Brick(self.board,self. user, self.bricks))
+
+context = Context()
 
 graphic_buffer = [
-    [' ' for index_x in range(len(board.fields[index_y]))] for index_y in range(len(board.fields))
+    [' ' for index_x in range(len(context.board.fields[index_y]))] for index_y in range(len(context.board.fields))
 ]
 
 while True:
-    board.draw(graphic_buffer)
-    for brick in bricks:
-        brick.draw(graphic_buffer)
-    user.draw(graphic_buffer)
-    for bomb in bombs:
-        bomb.draw(graphic_buffer, bombs, user)
+    context.board.draw(graphic_buffer, context)
+    for brick in context.bricks:
+        brick.draw(graphic_buffer, context)
+    context.user.draw(graphic_buffer, context)
+    for bomb in context.bombs:
+        bomb.draw(graphic_buffer, context)
+    if context.portal is not None:
+        context.portal.draw(graphic_buffer, context)
     for row in graphic_buffer:
         print(' '.join(row))
-    print('Lives: {}, Points: {}'.format(user.life, user.points))
-    for bomb in bombs:
+    print('Lives: {}, Points: {}'.format(context.user.life, context.user.points))
+    for bomb in context.bombs:
         print('Bomb time: {}'.format(bomb.time))
-    user.make_step(board, bricks)
-    for bomb in bombs:
-        bomb.make_step(bombs, dead_list, bricks)
-    for element in dead_list:
+    context.user.make_step(context)
+    for bomb in context.bombs:
+        bomb.make_step(context)
+    for element in context.dead_list:
         if isinstance(element, Bomb):
-            bombs.remove(element)
+            context.bombs.remove(element)
         if isinstance(element, Brick):
-            bricks.remove(element)
-    dead_list = []
+            context.bricks.remove(element)
+    context.dead_list = []
 
 
 
 # TODO:
-# bomb decrement number of lives
-# points
-# game over
 # portal ending level
 # level
+# game over
 # artifacts - bigger range of bomb explosion, add life, more points
 # monster - random place, random number
+# kill monster
+# points for monster
+# unit testy
+

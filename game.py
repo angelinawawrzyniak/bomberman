b3e8003911cb564bf5f391c965d8f4b43a1bac75
Angelina Wawrzyniak <angelina.wawrzyniak92@gmail.com>
2017-10-20 00:40:04 +0200

    Game over when user.life == 0 Randomly adding portal and additional life

--- game.py
+++ game.py
@@ -1,4 +1,9 @@
-from random import randint
+from abc import abstractmethod
+from random import randint, random
+
+
+class GameOverError(Exception):
+    pass
 
 
 class Board:
@@ -34,11 +39,11 @@ class Board:
 
 class User:
 
-    def __init__(self, step, board):
+    def __init__(self, step, board, life, points):
         self._set_up_user(board)
         self.step = step
-        self.points = 0
-        self.life = 3
+        self.points = points
+        self.life = life
 
     def _set_up_user(self, board):
         while True:
@@ -150,37 +155,71 @@ class Bomb:
                             context.dead_list.append(brick)
                             self.add_point(context.user, brick)
                             if context.portal is None:
-                                context.portal = Portal(brick.y, brick.x)
+                                if random() <= 1/len(context.bricks):
+                                    context.portal = Portal(brick.y, brick.x)
+                            if context.portal is None:
+                                if random() <= 0.2:
+                                    context.artifacts.append(Life(brick.y, brick.x))
+
             offsets.append((0, 0))
             for (offset_y, offset_x) in offsets:
                 if (context.user.y, context.user.x) == (self.y + offset_y, self.x + offset_x):
                     context.user.life -= 1
+                    if context.user.life == 0:
+                        raise GameOverError('GAME OVER')
                     break
 
     def add_point(self, user, destroyed_objects):
         user.points += destroyed_objects.REWARD
 
 
+class Artifact:
+    SYMBOL = '?'
+
+    def __init__(self, y, x):
+        self.x = x
+        self.y = y
+
+    def draw(self, graphic_buffer, context):
+        graphic_buffer[self.y][self.x] = self.SYMBOL
+
+    @abstractmethod
+    def make_step(self, context):
+        pass
+
+
+class Life(Artifact):
+
+    SYMBOL = 'L'
+
+    def make_step(self, context):
+        if (context.user.y, context.user.x) == (artifact.y, artifact.x):
+            context.dead_list.append(artifact)
+            context.user.life += 1
+
+
 class Context:
 
     def __init__(self):
         self.game_level = 1
-        self._reset_context()
+        self._reset_context(3, 0)
 
     def level_up(self):
         self.game_level += 1
-        self._reset_context()
+        self._reset_context(self.user.life, self.user.points)
 
-    def _reset_context(self):
+    def _reset_context(self, user_life, user_points):
         self.board = Board()
-        self.user = User(1, self.board)
+        self.user = User(1, self.board, user_life, user_points)
         self.bricks = []
         self.portal = None
         self.bombs = []
         self.dead_list =[]
+        self.game_over = False
+        self.artifacts = []
 
         for _ in range(20):
-            self.bricks.append(Brick(self.board,self. user, self.bricks))
+            self.bricks.append(Brick(self.board, self.user, self.bricks))
 
 
 def draw_scene(context, graphic_buffer):
@@ -192,12 +231,20 @@ def draw_scene(context, graphic_buffer):
         bomb.draw(graphic_buffer, context)
     if context.portal is not None:
         context.portal.draw(graphic_buffer, context)
+    for artifact in context.artifacts:
+        artifact.draw(graphic_buffer, context)
+    if context.game_over:
+        letters = list('GAME OVER')
+        offset = int((len(graphic_buffer[0]) - len(letters)) / 2)
+        for x in range(0, len(letters)):
+            graphic_buffer[int(len(graphic_buffer) / 2)][x + offset] = letters[x]
     for row in graphic_buffer:
         print(' '.join(row))
     print('Level: {}, Lives: {}, Points: {}'.format(context.game_level, context.user.life,
                                                     context.user.points))
     for bomb in context.bombs:
         print('Bomb time: {}'.format(bomb.time))
+
 
 context = Context()
 graphic_buffer = [
@@ -208,24 +255,32 @@ graphic_buffer = [
 while True:
     draw_scene(context, graphic_buffer)
     context.user.make_step(context)
+    for artifact in context.artifacts:
+        artifact.make_step(context)
     if context.portal is not None:
         if (context.user.y, context.user.x) == (context.portal.y, context.portal.x):
             context.level_up()
             continue
-    for bomb in context.bombs:
-        bomb.make_step(context)
+    try:
+        for bomb in context.bombs:
+            bomb.make_step(context)
+    except GameOverError as error:
+        context.game_over = True
+        draw_scene(context, graphic_buffer)
+        break
     for element in context.dead_list:
         if isinstance(element, Bomb):
             context.bombs.remove(element)
         if isinstance(element, Brick):
             context.bricks.remove(element)
+        if isinstance(element, Life):
+            context.artifacts.remove(element)
     context.dead_list = []
 
 
 
 # TODO:
-# game over
-# artifacts - bigger range of bomb explosion, add life, more points
+# artifacts - bigger range of bomb explosion, more points
 # monster - random place, random number
 # kill monster
 # points for monster
